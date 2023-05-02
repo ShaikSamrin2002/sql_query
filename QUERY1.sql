7c9ce70e3b8d1d6bd7f34636c623fbafa85fe113
ShaikSamrin2002 <130962382+ShaikSamrin2002@users.noreply.github.com>
2023-05-02 12:51:35 +0530

    Add files via upload

---
+++ QUERY1.sql
@@ -0,0 +1,22 @@
+-- Creating database and its name.
+-- create database sam;
+
+-- create table student(
+-- student_id int primary key not null,
+-- student_name varchar(30) not null,
+-- student_contact int not null,
+-- student_dept varchar(25) not null
+-- );
+-- select * from student;
+
+-- insert into student(student_id, student_name, student_contact,  student_dept)
+-- values(102,"samrin",9089248,"ECE")
+
+
+insert into student values (200,"zaibu",43556,"EXE"),(300,"farha",84398,"MECH");
+
+select * from student;
+
+
+
+
